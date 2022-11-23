faee668c5b414c8df0c716eecb8735f555542add
Shivansh070707 <Shivansh0301@gmail.com>
2022-11-23 15:50:11 +0530

    deployed using truffle dashboard

--- Task 1/hardhat.config.js
+++ Task 1/hardhat.config.js
@@ -1,8 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 
-const MATIC_PRIVATE_KEY =
-  "83e2a2a92cf8b81f68dd13aa795b1be2cff51c2a444bdc63d84026736a821f60";
+// const MATIC_PRIVATE_KEY =
+//   "83e2a2a92cf8b81f68dd13aa795b1be2cff51c2a444bdc63d84026736a821f60";
 
 module.exports = {
   solidity: {
@@ -22,8 +22,11 @@ module.exports = {
       url: "https://wiser-proud-sea.matic-testnet.discover.quiknode.pro/02c8f3feafa3227e22b53fc40e844333a15a6290/",
 
       chainId: 80001,
-      accounts: [MATIC_PRIVATE_KEY],
+   
     },
+    "truffle-dashboard": {
+      url: "http://localhost:24012/rpc"
+    }
   },
   etherscan: {
     apiKey: {
--- Task 1/scripts/deploy.js
+++ Task 1/scripts/deploy.js
@@ -2,6 +2,9 @@
 const hre = require("hardhat");
 //token deployed to 0x5Aa239FC81Ef17Dc25989C67324d8D55aA794Bb5
 //NFT deployed to 0x9e24E856EC665EbB4B11C5F2D301835F7f445751
+//
+//token deployed to 0x48324c858a0a35eB42bB77ABEb4dae68Af749098 using truffle dashboard
+//NFT deployed to 0x5039f99761A971b1ebb5487c66dEC12cD4A7a97c
 
 
 async function main() {
@@ -48,11 +51,11 @@ async function verify(){
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
-verify().catch((error) => {
+main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+// verify().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
