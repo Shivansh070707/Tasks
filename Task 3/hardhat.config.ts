3bec241e0e0b1636a50d65bf22983b39829999ee
Shivansh070707 <Shivansh0301@gmail.com>
2022-12-20 13:18:10 +0530

    'initial'

---
+++ Task 3/hardhat.config.ts
@@ -0,0 +1,56 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+import("hardhat-contract-sizer");
+import * as dotenv from "dotenv";
+dotenv.config();
+const { ALCHEMY_API_KEY, GOERLI_API_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.14",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+        blockNumber: 16139820,
+      },
+    },
+    "truffle-dashboard": {
+      url: "http://localhost:24012/rpc",
+    },
+  },
+  etherscan: {
+    apiKey: GOERLI_API_KEY,
+  },
+
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: false,
+    strict: true,
+    only: [],
+  },
+};
+export default config;
